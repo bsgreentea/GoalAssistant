d55fbdac8575746eec8dc62cbb8ef96a0bf2281c
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2019-12-10 20:11:21 +0900

    아이콘 추가 및 위치 조정
    텍스트 박스에 잠금 아이콘 추가

--- Locker/app/src/main/java/com/greentea/locker/AppInfoActivity.java
+++ Locker/app/src/main/java/com/greentea/locker/AppInfoActivity.java
@@ -96,11 +96,13 @@ public class AppInfoActivity extends AppCompatActivity {
 
         if(versionFlag){
             tasteView.setText("매운맛");
+            tasteView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_outline_white_24dp,0,0,0);
             tasteView.setBackgroundColor(0xFFC71585);
             tasteView.setTextColor(0xFFFFFFFF);
         }
         else{
             tasteView.setText("순한맛");
+            tasteView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_open_black_24dp,0,0,0);
             tasteView.setBackgroundColor(0xFFFFFFFF);
             tasteView.setTextColor(0xFF000000);
         }
@@ -110,18 +112,18 @@ public class AppInfoActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 if(versionFlag){
-//                    tasteView.setText("순한맛");
+                    tasteView.setText("순한맛");
                     tasteView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_open_black_24dp,0,0,0);
                     tasteView.setBackgroundColor(0xFFFFFFFF);
-//                    tasteView.setTextColor(0xFF000000);
+                    tasteView.setTextColor(0xFF000000);
                     Toast.makeText(getApplicationContext(), "순한맛으로 변경합니다.", Toast.LENGTH_SHORT).show();
                     versionFlag = false;
                 }
                 else{
-//                    tasteView.setText("매운맛");
+                    tasteView.setText("매운맛");
                     tasteView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_outline_white_24dp,0,0,0);
                     tasteView.setBackgroundColor(0xFFC71585);
-//                    tasteView.setTextColor(0xFFFFFFFF);
+                    tasteView.setTextColor(0xFFFFFFFF);
                     Toast.makeText(getApplicationContext(), "지정된 범위에 진입시 앱 사용이 불가합니다.", Toast.LENGTH_SHORT).show();
                     versionFlag = true;
                 }
